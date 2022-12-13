4c15952b63afe0ef30d638c412e83e03e7ff0732
Thomas McCluskey <twm9711@nyu.edu>
2022-12-13 00:55:30 -0500

    second test
    test

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".auto-type", {
-    strings: ["Motion Designer", "Digital Fabricator", "3D Artist", "UX Designer"],
+    strings: ["Motion Designer", "Digital Fabricator", "3D Artist", "Video Artist"],
     typeSpeed: 75,
     backSpeed: 100,
     loop: true
@@ -11,22 +11,5 @@ window.addEventListener("load", function () {
     loader.style.display = "none";
 })
 
-const content = document.querySelector('section');
-
-let currentPosition = window.pageYOffset;
-
-function skewEffect() {
-    const newPosition = window.pageYOffset;
-    const dif = newPosition - currentPosition;
-
-    let skew = dif * .15;
-    let rotate = dif * .025;
-
-    content.style.transform = `skewY(${skew}deg) rotateY(${rotate}deg)`;
-    currentPosition = newPosition;
-    requestAnimationFrame(skewEffect);
-}
-
-skewEffect();
 
 
